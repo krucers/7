f4adbacc61b85e85d6313be628237e834a7a14ad
dypni <dyupin-2025@mail.ru>
2025-12-12 16:03:33 +0500

    все

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,61 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    var dog = 8.0;
+        System.out.println(dog);
+    var cat = 3.6;
+        System.out.println(cat);
+    var paper = 763789;
+        System.out.println(paper);
+    dog = dog + 4;
+        System.out.println(dog);
+    cat = cat + 4;
+        System.out.println(cat);
+    paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+    var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(friend);
+        frog  = frog / 3.5;
+        System.out.println(friend);
+        frog = frog + 4;
+        System.out.println(friend);
+    var боец1 = 78.2;
+        System.out.println(боец1);
+    var боец2 = 82.7;
+        System.out.println(боец2);
+    var result = боец1 + боец2;
+        System.out.println(result);
+    var result1 = боец2 - боец1;
+        System.out.println(result1);
+    var result2 = боец2 % боец1;
+        System.out.println(result2);
+    var время = 640;
+        System.out.println(время);
+    var время1 = 8;
+        System.out.println(время1);
+    var работник = время / время1;
+        System.out.println("Всего работников в компании —" +работник+ "человек");
+    var работник2 = работник + 94;
+        System.out.println(работник2);
+    var время3 = работник2 * время1;
+        System.out.println(время3);
+        System.out.println("Если в компании работает" +работник2+"человек, то всего"+время3+"часов работы может быть поделено между сотрудниками");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
     }
 }
