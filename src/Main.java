7509a607f44af207b56c36f6ed76effbae33b71a
dypni <dyupin-2025@mail.ru>
2025-12-24 16:15:27 +0500

    все4

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    var dog = 8.0;
+        var dog = 8.0;
         System.out.println(dog);
-    var cat = 3.6;
+        var cat = 3.6;
         System.out.println(cat);
-    var paper = 763789;
+        var paper = 763789;
         System.out.println(paper);
-    dog = dog + 4;
+        dog = dog + 4;
         System.out.println(dog);
-    cat = cat + 4;
+        cat = cat + 4;
         System.out.println(cat);
-    paper = paper + 4;
+        paper = paper + 4;
         System.out.println(paper);
         dog = dog - 3.5;
         System.out.println(dog);
@@ -20,42 +20,41 @@ public class Main {
         System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
-    var friend = 19;
+        var friend = 19;
         System.out.println(friend);
         friend = friend * 2;
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
-    var frog = 3.5;
+        var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
         System.out.println(frog);
-        frog  = frog / 3.5;
+        frog = frog / 3.5;
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
-    var fighter1 = 78.2;
+        var fighter1 = 78.2;
         System.out.println(fighter1);
-    var fighter2 = 82.7;
+        var fighter2 = 82.7;
         System.out.println(fighter2);
-    var result = fighter1 + fighter2;
+        var result = fighter1 + fighter2;
         System.out.println(result);
-    var result1 = fighter2 - fighter1;
+        var result1 = fighter2 - fighter1;
         System.out.println(result1);
-    var result2 = fighter2 % fighter1;
+        var result2 = fighter2 % fighter1;
         System.out.println(result2);
-    var time = 640;
+        var time = 640;
         System.out.println(time);
-    var time1 = 8;
+        var time1 = 8;
         System.out.println(time1);
-    var worker = time / time1;
-        System.out.println("Всего работников в компании —"  +worker+  "человек");
-    var worker2 = worker + 94;
+        var worker = time / time1;
+        System.out.println("Всего работников в компании — " +worker+ " человек");
+        var worker2 = worker + 94;
         System.out.println(worker2);
-    var time3 = worker2 * time1;
+        var time3 = worker2 * time1;
         System.out.println(time3);
-        System.out.println("Если в компании работает"  +worker2+  "человек, то всего" +time3+ "часов работы может быть поделено между сотрудниками");
-
+        System.out.println("Если в компании работает " +worker2+ " человек, то всего " +time3+ " часов работы может быть поделено между сотрудниками");
 
 
     }
